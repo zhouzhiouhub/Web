178ca87d5970174ddbc48aafc150b87f92ac3ac9
zhouzhiouhub <zhouzhiou9588@163.com>
2026-07-13 15:19:13 +0800

    chore: 移除不再使用的 stylelint 插件
    原因：
    - 为了优化构建速度，移除了不再使用的 stylelint 插件。
    
    变更：
    - 从 package.json 和 vite.config.ts 中删除了 stylelint 相关配置。
    
    影响：
    - 可能影响样式检查，但在生产环境中不会执行 stylelint。
    
    变更文件：
    - package.json
    - vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
-import stylelint from 'vite-plugin-stylelint';
 import svgLoader from 'vite-svg-loader';
 import autoImport from 'unplugin-auto-import/dist/vite.js';
 import { resolve } from 'path';
@@ -16,7 +15,6 @@ export default ({ mode }: { mode: string }) => {
       vue(),
       // Only run ESLint and Stylelint in dev mode for faster builds
       isDev && eslint({ cache: false }),
-      isDev && stylelint(),
       svgLoader(),
       autoImport({
         imports: [
